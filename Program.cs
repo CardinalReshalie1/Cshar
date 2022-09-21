412a45440d979d700eac756f44e5435827d767c6
konstantin <cardinalreshalie95@gmail.com>
2022-09-21 17:50:07 +0300

    sandra

--- Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("writeyourname ");
-string username = Console.ReadLine();
-Console.Write("Hello, ");
-Console.Write(username);
